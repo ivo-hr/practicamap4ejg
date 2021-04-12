f81b47d4a85c094b2943c692043104335f421166
Enrique Juan <enrijuan@ucm.es>
2021-04-12 11:30:35 +0200

    archivos iniciales pal repositorio

---
+++ Tablero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace prac2
+{
+    class Tablero
+    {
+        // contenido de las casillas
+        enum Casilla { Libre, Muro, Comida, Vitamina, MuroCelda };
+        // matriz de casillas (tablero)
+        Casilla[,] cas;
+        // representacion de los personajes (pacman y fantasmas)
+        struct Personaje
+        {
+            public Coor pos, dir, // posicion y direccion actual
+            ini; // posicion inicial (para fantasmas)
+        }
+        // vector de personajes, 0 es pacman y el resto fantasmas
+        Personaje[] pers;
+        // colores para los personajes
+        ConsoleColor[] colors = {ConsoleColor.DarkYellow, ConsoleColor.Red,
+        ConsoleColor.Magenta, ConsoleColor.Cyan, ConsoleColor.DarkBlue };
+        const int lapCarcelFantasmas = 3000; // retardo para quitar el muro a los fantasmas
+        int lapFantasmas; // tiempo restante para quitar el muro
+        int numComida;
+        // numero de casillas restantes con comida o vitamina
+        Random rnd; // generador de aleatorios
+                    // flag para mensajes de depuracion en consola
+        private bool Debug = true;
+    }
+}
+
